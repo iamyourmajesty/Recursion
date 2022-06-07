8e38bdfa91d5722f9591649f98e963c806f22dda
Prince Pandey <princepandey2784@gmail.com>
2022-06-07 19:01:19 +0530

    good eg of backtracking

---
+++ .vscode/all_binary_string_of_n_bits.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(string str,int n,int i)
+{
+    if(i>=n)
+    {
+        cout<<str<<endl;
+        return;
+    }
+    //putting 0 as bit
+    str.push_back('0');
+    solve(str,n,i+1);
+    //backtrack
+    str.pop_back();
+
+    //putting 1 as bit
+    str.push_back('1');
+    solve(str,n,i+1);
+}
+int main()
+{
+    int n=2;
+    string str="";
+    solve(str,n,0);
+    return 0;
+}
+/*
+Genrating all string of n
+eg-> 2
+    00
+    01
+    10
+    11
+        */
