20971c337b117b324488d15fa02e564184cd3f0f
Prince Pandey <princepandey2784@gmail.com>
2022-10-09 17:05:38 +0530

    medium leve -l 1

---
+++ Recursion part - 02/2_fast_power.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fast_power(int a,int b)
+{
+    //base case
+    if(b==0) return 1;
+    else if(b==1) return a;
+
+    //odd
+    if(b&1)
+    return a*fast_power(a*a,b>>1);
+    //else even
+    return fast_power(a*a,b>>1);
+}
+int main()
+{
+    int a = 2;
+    int b= 7;
+    cout<<fast_power(a,b);
+    return 0;
+}
+
+// since we are naming it as fast power ,though we must use iterative approach to make is more faster
